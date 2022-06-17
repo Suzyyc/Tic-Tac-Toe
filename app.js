168b374e53c55c55437a9ad06f9284367d40137b
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-06-17 21:18:45 +1000

    players x and o

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 const container = document.querySelector(".container");
+
+const grids = document.querySelectorAll("#ttt");
+
+// const text = document.querySelectorAll(".grid");
+// for (let click of text) {
+//     text[].addEventListener("click", () => {
+//     text.innerHTML = "X";
+//   });
+// }
+
+const playerX = {
+  name: "X",
+  turn: () => {},
+  wins: 0,
+};
+
+const playerO = {
+  name: "O",
+  turn: () => {},
+  wins: 0,
+};
+
+let currentPlayer = playerX;
+
+//Player X
+document.querySelectorAll(".grid").forEach((text) => {
+  text.addEventListener("click", () => {
+    text.innerHTML = currentPlayer.name;
+    currentPlayer = playerO;
+  });
+});
+
+//Player O
+
+// function to see who is playing
+// define who the players Are`
+
+//=======================================
+// to restart the whole game/page
+//=======================================
+document.querySelector(".restartButton").addEventListener("click", () => {
+  console.log("Run bitch");
+});
