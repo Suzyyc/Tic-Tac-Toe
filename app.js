76a00bbfb4767b81f1618c9ab445131accbe3807
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-06-18 17:23:42 +1000

    wins aren't true for all

--- app.js
+++ app.js
@@ -25,47 +25,6 @@ const playerO = {
 
 let currentPlayer = playerX;
 
-//Player X
-document.querySelectorAll(".grid").forEach((text) => {
-  text.addEventListener("click", () => {
-    text.innerHTML = currentPlayer.name;
-    // Turn 1
-    // X clicks on cell 2
-    // [2]
-    // Switch player to player0
-
-    // get cell number once clicked
-    currentPlayer.combination.push(parseInt(text.getAttribute("id")));
-
-    // get winning winningCombos
-
-    // if (currentPlayer.combination === winningCombos) {
-    //   console.log("you win");
-    // }
-
-    for (let win of winningCombos) {
-      console.log(compareArrays(currentPlayer.combination, win));
-    }
-
-    console.log(currentPlayer.combination);
-    console.log(winningCombos);
-    // currentPlayer =
-
-    // Turn 2
-    // 0 clicks
-    // Switch player to playerX
-
-    // Turn 3
-    // ... same as turn 1
-
-    if (currentPlayer === playerX) {
-      currentPlayer = playerO;
-    } else if (currentPlayer === playerO) {
-      currentPlayer = playerX;
-    }
-  });
-});
-
 const winningCombos = [
   [1, 2, 3],
   [4, 5, 6],
@@ -77,11 +36,65 @@ const winningCombos = [
   [3, 5, 7],
 ];
 
-let compareArrays = (a, b) => {
-  return a.sort().join() == b.sort().join();
+const compareArrays = (a, b) => {
+  const aString = a.sort().join();
+  const bString = b.sort().join();
+  // bStr = "123" exist in aStr = "12345"? subset
+  console.log(aString, bString);
+  return aString.indexOf(bString) !== -1 || bString.indexOf(aString) !== -1;
 };
 
-//Player O
+//Player X
+document.querySelectorAll(".grid").forEach((text) => {
+  text.addEventListener("click", () => {
+    // Option 1: Check if the id value exists in a player's combination array
+    // Option 2: Check if the HTML already has a value inside it
+    if (text.innerHTML === "") {
+      console.log("run code");
+      text.innerHTML = currentPlayer.name;
+
+      // Turn 1
+      // X clicks on cell 2
+      // [2]
+      // Switch player to player0
+
+      // get cell number once clicked
+      currentPlayer.combination.push(parseInt(text.getAttribute("id")));
+
+      // get winning winningCombos
+
+      // if (currentPlayer.combination === winningCombos) {
+      //   console.log("you win");
+      // }
+
+      if (currentPlayer.combination.length > 3) {
+        for (let win of winningCombos) {
+          const hasWon = compareArrays(currentPlayer.combination, win);
+          if (hasWon) console.log(hasWon);
+        }
+      }
+
+      // console.log(currentPlayer.combination);
+      // console.log(winningCombos);
+      // currentPlayer =
+
+      // Turn 2
+      // 0 clicks
+      // Switch player to playerX
+
+      // Turn 3
+      // ... same as turn 1
+
+      if (currentPlayer === playerX) {
+        currentPlayer = playerO;
+      } else if (currentPlayer === playerO) {
+        currentPlayer = playerX;
+      }
+    }
+  });
+});
+
+// if all 9 turns go and no one won. do a draw
 
 // function to see who is playing
 // define who the players Are`
